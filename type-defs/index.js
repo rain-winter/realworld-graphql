366fa2f304b34000aff8cef48a6499d4085d9da2
rain <3100406687@qq.com>
2022-02-15 18:09:16 +0800

    设计登录的schema

--- type-defs/index.js
+++ type-defs/index.js
@@ -3,8 +3,35 @@
  */
 const { gql } = require('apollo-server-express')
 const typeDefs = gql`
+  type User {
+    email: String!,
+    token: String,
+    username: String!,
+    bio: String,
+    image: String
+  }
+  # 返回这个对象
+  type UserPayLoad {
+    user: User
+  }
+
   type Query {
     foo: String
+  }
+
+  input LoginInput {
+    email: String!,
+    password: String!
+  }
+  input CreateUserInput {
+    username: String!,
+    email: String!,
+    password: String!
+  }
+
+  type Mutation {
+    login (user: LoginInput!): UserPayLoad
+    createUser (user: CreateUserInput): UserPayLoad
   }
 `
 module.exports = typeDefs
