0fea6d49e67d809d7617b455ce7dcf880489ae3b
rain <3100406687@qq.com>
2022-02-16 17:34:14 +0800

    用户登录

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,6 +1,20 @@
 const { UserInputError } = require('apollo-server-express')
 const jwt = require('../util/jwt')
 const { jwtSecret } = require('../config/config.default')
+const md5 = require('../util/md5')
+
+const generateToken = (userId) => {
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    jwtSecret,
+    {
+      expiresIn: 60 * 60 * 24,
+    }
+  )
+  return token
+}
 
 const resolvers = {
   Query: {
@@ -22,18 +36,32 @@ const resolvers = {
       }
       // 保存用户
       const userData = await users.saveUser(user)
-      console.log(userData)
       // 生成token
-      const token = await jwt.sign(
-        {
-          userId: userData._id,
-        },
-        jwtSecret,
-        {
-          expiresIn: 60 * 60 * 24,
-        }
-      )
+      const token = generateToken(userData._id)
       // 返回客户端
+      return {
+        user: {
+          ...userData.toObject(),
+          token,
+        },
+      }
+    },
+
+    async login(parent, { user }, { dataSources }) {
+      // 用户是否存在（email）
+      const users = dataSources.users
+
+      const userData = await users.findByEmail(user.email)
+      if (!userData) {
+        throw new UserInputError('邮箱不存在')
+      }
+      // 校验密码
+      if (md5(user.password) != userData.password) {
+        throw new UserInputError('密码错误')
+      }
+
+      const token = generateToken(userData._id)
+      // 发送成功相应
       return {
         user: {
           ...userData.toObject(),
--- util/jwt.js
+++ util/jwt.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken')
 const { promisify } = require('util')
 
+const { jwtSecret } = require('../config/config.default')
+
 exports.sign = promisify(jwt.sign)
 exports.verify = promisify(jwt.verify)
 exports.decode = promisify(jwt.decode)
+
